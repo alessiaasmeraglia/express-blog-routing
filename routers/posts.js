c37864de1f6bf9b5de3c958c86bf7d2ba9c0de29
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-22 17:29:52 +0200

    Aggiunte rotte CRUD

---
+++ routers/posts.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+    index,
+    show,
+    create,
+    update,
+    destroy,
+} from "../controllers/posts.js";
+
+const router = express.Router();
+
+router.get("/", index);
+router.get("/:id", show);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", destroy);
+
+export default router;
